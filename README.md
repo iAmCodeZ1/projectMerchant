92c71512ca5a208dcbec245ceeaa5e27ade2597d
Ace <iamcodez1@gmail.com>
2020-06-14 18:50:24 +0800

    clean commit for cli update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MarchantWebApp
+# MerchantWebApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.5.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('marchantWebApp app is running!');
+    expect(page.getTitleText()).toEqual('merchantWebApp app is running!');
   });
 
   afterEach(async () => {
