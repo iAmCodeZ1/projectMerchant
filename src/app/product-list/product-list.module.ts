323012497354a368a3cd0fefb0c773cc23e3840c
Ace <iamcodez1@gmail.com>
2020-06-21 19:33:20 +0800

    Added ellipsis and optimized mobile version

--- src/app/product-list/product-list.module.ts
+++ src/app/product-list/product-list.module.ts
@@ -5,10 +5,11 @@ import { ProductListRoutingModule } from './product-list-routing.module';
 import { MaterialModule } from '../material/material.module';
 
 import { ProductListComponent } from './product-list/product-list.component';
+import { EllipsisPipe } from './ellipsis.pipe';
 
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductListComponent, EllipsisPipe],
   imports: [
     CommonModule,
     ProductListRoutingModule,
