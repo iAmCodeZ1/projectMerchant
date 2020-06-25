c847c0d441f844615518942a1d37f1a4823ebaff
Ace <iamcodez1@gmail.com>
2020-06-25 18:45:10 +0800

    Added new color scheme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    scrollPositionRestoration: 'enabled'
+    scrollPositionRestoration: 'enabled',
+    useHash: true
   })],
   exports: [RouterModule]
 })
